43697ae2416072536c9c9fff250cebfa19c93e3c
De Pace <107199019+PaceDE@users.noreply.github.com>
2024-08-25 08:00:15 +0545

    Add files via upload

---
+++ 14_nonLinear_select.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#define MAX 100
+
+int Select_NonLinear(int a[], int k, int n) {
+    int i, j, temp, min;
+    for(i = 0; i < k; i++) {
+        min = i;
+        for(j = i + 1; j < n; j++) {
+            if(a[j] < a[min])
+                min = j;
+        }
+        temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+    }
+    return a[k - 1];
+}
+
+int main() {
+    int a[MAX], n, i, k, kthSmallest;
+    printf("Enter the number of Data item: ");
+    scanf("%d", &n);
+    printf("Enter the elements: ");
+    for(i = 0; i < n; ++i)
+        scanf("%d", &a[i]);
+    printf("Enter the position to find: ");
+    scanf("%d", &k);
+    kthSmallest = Select_NonLinear(a, k, n);
+    printf("%dth smallest: %d\n", k, kthSmallest);
+    return 0;
+}
